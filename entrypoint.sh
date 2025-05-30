6a23077b1d5dedca7279dc1ce5efd2e5ac8b5461
Daniel Peacock <64603482+DanielPeacock@users.noreply.github.com>
2025-05-30 10:33:46 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,18 @@
 #!/bin/bash -eu
 
-find $INPUT_INPUTS -name "*.tex" -o -name "*.bib" -o -name "*.cls" -o -name "*.sty" | xargs -r tex-fmt $INPUT_OPTS
+#!/bin/bash -euo pipefail
+
+FILES=$(find "$INPUT_INPUTS" \( -name "*.tex" -o -name "*.bib" -o -name "*.cls" -o -name "*.sty" \))
+
+if [[ -z "$FILES" ]]; then
+  echo "No files found to check"
+  exit 0
+fi
+
+FAIL=0
+for file in $FILES; do
+  tex-fmt $INPUT_OPTS "$file" || FAIL=1
+done
+
+exit $FAIL
+
